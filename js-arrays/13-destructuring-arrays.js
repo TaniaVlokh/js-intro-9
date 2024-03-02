46c37d8f510365f2ad4f041ff72e6977a0202794
TaniaVlokh <taniavlokh0204@gmail.com>
2024-03-02 10:25:12 -0600

    Loop changes

--- js-arrays/13-destructuring-arrays.js
+++ js-arrays/13-destructuring-arrays.js
@@ -12,8 +12,29 @@ const [ fav01, fav02, fav03] = favMovies;    // const [ , fav02, fav03] -> put '
 console.log(fav1);
 console.log(fav01);
 
-
 const credentials = ['TechGlobalSchool', 'Test123', 'techglobal@gmail.com'];
 const [ userName, password, email ] = credentials;
 
 console.log(`Mu username  is ${userName}, my password is ${password}, my email is ${email}`);
+
+function middleWay(a, b){
+    let[one,aNum, gig] = a;
+    let[gth,bNum,juu ] = b;
+  return aNum, bNum;
+  }
+  const arr1 = [0,3,4];
+  
+  
+
+  function has23(nums){
+    return nums.includes(2) || nums.includes(3);
+  }
+  console.log(has23(arr1));
+
+  function fix23(nums){
+    for(let i = 1; i < nums.length; i++){
+      if(nums[i-1] === 2 && (nums[i] === 3) {
+        nums[i] = 0;
+         }
+    return nums;
+  }
--- js-loops/02-loops-practice.js
+++ js-loops/02-loops-practice.js
@@ -1,3 +1,5 @@
+const {generateRandomNumber} = require('../utils/MathHelper.js');
+
 // Task -1 
 for(let i = 100; i >= 0; i--){
 console.log(i);
@@ -34,3 +36,77 @@ for(let i = 10; i <= 15; i++){
 console.log(sum);
 
 // Task - 6
+//let school = 'TechGlobal School';
+//let n = school.length;
+
+for(let i = 0; i < n; i++){
+console.log(school[i])
+}
+
+// Task - 7
+let school = 'TechGlobal School';
+let n = school.length;
+let count = 0;
+
+for(let i = 0; i < n; i++){
+    if(school[i] === 'o') 
+    count++;
+    }
+console.log(count);
+
+// another approach
+sconsole.lo(chool.split('').filter((element) => element.toLowerCase() === 'o').length);
+
+// Task - 8
+const {generateRandomNumber} = require('../utils/MathHelper.js');
+
+let ran = generateRandomNumber(10, 20);
+console.log(ran);
+
+for(let i = 0; i <= ran; i++){
+    if(ran % 2 !== 0) console.log(i);
+}
+
+// let num1  = generateRandomNumber(10, 20);
+// let num2  = generateRandomNumber(10, 20);
+
+//nums.sort((a, b) => a - b);
+// console.log(nums, '=========');  
+
+
+/*
+Print all the numbers between 2 random numbers in the range of 1 to 10 (both inclusive) in ascending order
+
+r1 r2
+7, 5    -> 5 6 7
+4, 8    -> 4 5 6 7 8
+5, 5    -> 5
+*/
+
+let r1 = generateRandomNumber(1, 10);
+let r2 = generateRandomNumber(1, 10); 
+console.log(r1, r2);
+
+
+let min = Math.min(r1, r2);
+let max = Math.max(r1, r2);
+
+let arr = [];
+for(let i = min; i <= max; i++){
+arr.push(i);
+}
+console.log(arr);
+
+
+
+const {generateRandomNumber} = require('../utils/MathHelper.js');
+
+let random = generateRandomNumber(1, 10);
+console.log(random);
+
+let product = 1;
+
+for(let i = 1; i <= random; i++){
+product *= i;
+}
+console.log(product);
